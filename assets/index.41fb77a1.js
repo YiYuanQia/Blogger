bdaa157cbfc18177a7bf11a284e2a77d21507451
YiYuanQia <YiYuanQia@users.noreply.github.com>
2023-01-22 02:17:57 +0000

    deploy: 03714d97a268e463c15b05ac3fd242da1fe30957

--- assets/index.41fb77a1.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./index.9747877a.js";const a=s({nickname:"--",phone:"--",sex:"--",emailaddress:"--",password:"--"});export{a as s};
---
+++ assets/index.7e178fcd.js
@@ -0,0 +1,1 @@
+import{d as s}from"./index.face8712.js";const a=s({nickname:"--",phone:"--",sex:"--",emailaddress:"--",password:"--"});export{a as s};
