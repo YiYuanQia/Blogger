90559c45a24456ec22c8c210fa5e29750de0e9bb
YiYuanQia <YiYuanQia@users.noreply.github.com>
2023-02-08 07:58:05 +0000

    deploy: e800fea76a5a0146e94f6f619c2c1e9b23b81660

--- assets/index.9285e75a.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./index.a778bde6.js";const a=s({nickname:"--",phone:"--",sex:"--",emailaddress:"--",password:"--"});export{a as s};
---
+++ assets/index.bf2ef14a.js
@@ -0,0 +1,1 @@
+import{d as s}from"./index.da18f286.js";const a=s({nickname:"--",phone:"--",sex:"--",emailaddress:"--",password:"--"});export{a as s};
