b3fd2dea814a6fdc792a3092a189d42fe7d21a13
YiYuanQia <YiYuanQia@users.noreply.github.com>
2023-02-27 16:49:44 +0000

    deploy: 2a5f3f68399ca3b2f12ceb4b32b43cef22ac43a1

---
+++ assets/index.71a58e6b.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index.eccb33de.js";const t=o({uid:"",token:""});export{t as s};
--- assets/index.bf2ef14a.js
+++
@@ -1,1 +0,0 @@
-import{d as s}from"./index.da18f286.js";const a=s({nickname:"--",phone:"--",sex:"--",emailaddress:"--",password:"--"});export{a as s};
